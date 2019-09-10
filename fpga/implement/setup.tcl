b3f0fd6477b1981cc97301ad1bd702f44cd75db4
hdlguy <hdlguy@hotmail.com>
2019-09-09 20:03:36 -0400

    redid system.tcl using zedboard definition file. hopefully that will make petalinux boot.

--- fpga/implement/setup.tcl
+++ fpga/implement/setup.tcl
@@ -3,8 +3,8 @@ close_project -quiet
 file delete -force proj.xpr *.os *.jou *.log proj.srcs proj.cache proj.runs
 #
 create_project -force proj 
-#set_property board_part em.avnet.com:zed:part0:1.3 [current_project]
-set_property part xc7z020clg400-1 [current_project]
+set_property board_part em.avnet.com:zed:part0:1.4 [current_project]
+#set_property part xc7z020clg400-1 [current_project]
 set_property target_language Verilog [current_project]
 set_property default_lib work [current_project]
 load_features ipintegrator
