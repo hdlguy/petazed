a26102dda5d75b0424018cadb3a22e036ec3bb41
pete <peter.dudley@precisionplanting.com>
2022-07-11 16:23:11 -0400

    compiling with Vivado 2022.1

--- fpga/implement/setup.tcl
+++ fpga/implement/setup.tcl
@@ -3,15 +3,12 @@ close_project -quiet
 file delete -force proj.xpr *.os *.jou *.log proj.srcs proj.cache proj.runs
 #
 create_project -force proj 
-set_property board_part em.avnet.com:zed:part0:1.4 [current_project]
-#set_property part xc7z020clg400-1 [current_project]
+#set_property board_part em.avnet.com:zed:part0:1.4 [current_project]
+set_property part xc7z020clg400-1 [current_project]
 set_property target_language Verilog [current_project]
 set_property default_lib work [current_project]
 load_features ipintegrator
 tclapp::install ultrafast -quiet
-
-#file delete -force ./ip
-#file mkdir ./ip
 
 #read_ip ../source/fader2/prod_ila/prod_ila.xci
 
@@ -23,20 +20,13 @@ source ../source/system.tcl
 generate_target {synthesis implementation} [get_files ./proj.srcs/sources_1/bd/system/system.bd]
 set_property synth_checkpoint_mode None    [get_files ./proj.srcs/sources_1/bd/system/system.bd]
 
-#write_hwdef -force -verbose ./results/system.hdf
 
-# Read in the hdl source.
 read_verilog -sv  [glob ../source/top.sv]
 
 read_xdc ../source/top.xdc
-#read_xdc ../source/zed_system_constr.xdc
-
-#read_xdc ../source/late.xdc
-#set_property used_in_synthesis false [get_files ../source/late.xdc]
 
 close_project
 
-#########################
 
 
 
