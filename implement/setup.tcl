ff57860e80f12bbf49fa3eb660e7bdcd57326f6e
hdlguy <hdlguy@hdlguy.com>
2025-03-02 11:50:50 -0500

    changed DDR part type in IPI, now Vitis app runs from DDR

--- implement/setup.tcl
+++ implement/setup.tcl
@@ -3,7 +3,8 @@ close_project -quiet
 file delete -force proj.xpr *.os *.jou *.log proj.srcs proj.cache proj.runs
 #
 create_project -force proj 
-set_property board_part avnet-tria:microzed_7020:part0:1.4 [current_project]
+set_property part xc7z020clg400-1 [current_project]
+#set_property board_part avnet-tria:microzed_7020:part0:1.4 [current_project]
 #set_property board_part avnet.com:zedboard:part0:1.4 [current_project]
 #set_property part xc7z020clg400-1 [current_project]
 set_property target_language Verilog [current_project]
@@ -13,8 +14,8 @@ tclapp::install ultrafast -quiet
 
 #read_ip ../source/fader2/prod_ila/prod_ila.xci
 
-upgrade_ip -quiet  [get_ips *]
-generate_target {all} [get_ips *]
+#upgrade_ip -quiet  [get_ips *]
+#generate_target {all} [get_ips *]
 
 # make the Zynq block diagram
 source ../source/system.tcl
--- vitis/src/hello1/test.c
+++ vitis/src/hello1/test.c
@@ -5,7 +5,7 @@
 
 #define BRAM_SIZE (1 + XPAR_BRAM_0_HIGHADDR - XPAR_BRAM_0_BASEADDR) // bram size in bytes
 
-#define DDR_BLOCK_SIZE (4*1024)
+#define DDR_BLOCK_SIZE (4*1024*1024)
 uint32_t ddrptr[DDR_BLOCK_SIZE/4];
 
 
@@ -14,9 +14,10 @@ int main()
 
     uint32_t* bramptr = (uint32_t *)XPAR_BRAM_0_BASEADDR;
 
-    xil_printf("\n\rHello! bramptr = 0x%08x, BRAM_SIZE = 0x%04x\n\r", bramptr, BRAM_SIZE);
+    xil_printf("\n\rHello!\n\r");
 
     // test bram
+    xil_printf("bramptr = 0x%08x, BRAM_SIZE = 0x%04x\n\r", bramptr, BRAM_SIZE);
     srand(1);
     for (int i=0; i<BRAM_SIZE/4; i++) bramptr[i] = rand();
     uint32_t randval, readval;
@@ -30,6 +31,7 @@ int main()
     
 
     // test ddr
+    xil_printf("ddrptr = 0x%08x, DDR_BLOCK_SIZE = 0x%04x\n\r", ddrptr, DDR_BLOCK_SIZE);
     srand(1);
     for (int i=0; i<DDR_BLOCK_SIZE/4; i++) ddrptr[i] = rand();
     errors=0;
