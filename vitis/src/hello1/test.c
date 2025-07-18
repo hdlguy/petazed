1dc379dccca95eeed3bc375d864aa95e68d9451f
hdlguy <hdlguy@hdlguy.com>
2025-07-18 16:33:30 -0400

    hello1 tests memories on microzed

--- vitis/src/hello1/test.c
+++ vitis/src/hello1/test.c
@@ -3,48 +3,54 @@
 #include "xil_printf.h"
 #include "xparameters.h"
 
-#define BRAM_SIZE (1 + XPAR_BRAM_0_HIGHADDR - XPAR_BRAM_0_BASEADDR) // bram size in bytes
+#define BRAM_SIZE (1 + XPAR_AXI_BRAM_CTRL_0_HIGHADDR - XPAR_AXI_BRAM_CTRL_0_BASEADDR) // bram size in bytes
 
-#define DDR_BLOCK_SIZE (4*1024*1024)
+#define DDR_BLOCK_SIZE (32*1024*1024)
 uint32_t ddrptr[DDR_BLOCK_SIZE/4];
-
 
 int main()
 {
+    uint32_t* bramptr = (uint32_t *)XPAR_AXI_BRAM_CTRL_0_BASEADDR;
 
-    uint32_t* bramptr = (uint32_t *)XPAR_BRAM_0_BASEADDR;
+    uint32_t whilecount=0;
+    while(1) {
 
-    xil_printf("\n\rHello!\n\r");
+        xil_printf("\n\rHello!\n\r");
 
-    // test bram
-    xil_printf("bramptr = 0x%08x, BRAM_SIZE = 0x%04x\n\r", bramptr, BRAM_SIZE);
-    srand(1);
-    for (int i=0; i<BRAM_SIZE/4; i++) bramptr[i] = rand();
-    uint32_t randval, readval;
-    int errors=0;
-    srand(1);
-    for (int i=0; i<BRAM_SIZE/4; i++) {
-    	randval = rand();
-    	if (randval != bramptr[i]) errors++;
+        // test bram
+        xil_printf("%08d: bramptr = 0x%08x, BRAM_SIZE = 0x%04x\n\r", whilecount, bramptr, BRAM_SIZE);
+        srand(whilecount);
+        for (int i=0; i<BRAM_SIZE/4; i++) bramptr[i] = rand();
+        uint32_t randval, readval;
+        int errors=0;
+        srand(whilecount);
+        for (int i=0; i<BRAM_SIZE/4; i++) {
+            randval = rand();
+            if (randval != bramptr[i]) errors++;
+        }
+        xil_printf("bram test: errors = %d\n\r", errors);
+        
+
+        // test ddr
+        xil_printf("ddrptr = 0x%08x, DDR_BLOCK_SIZE = 0x%04x\n\r", ddrptr, DDR_BLOCK_SIZE);
+        srand(whilecount);
+        for (int i=0; i<DDR_BLOCK_SIZE/4; i++) ddrptr[i] = rand();
+        errors=0;
+        srand(whilecount);
+        for (int i=0; i<DDR_BLOCK_SIZE/4; i++) {
+            randval = rand();
+            readval = ddrptr[i];
+            if (randval != readval) {
+                errors++;
+                xil_printf("%d: %08x  %08x\n\r", i, randval, readval);
+            }
+        }
+        xil_printf("ddr test: errors = %d\n\r", errors);
+
+        for(int i=0; i<1000000; i++);
+        whilecount++;
+
     }
-    xil_printf("bram test: errors = %d\n\r", errors);
-    
-
-    // test ddr
-    xil_printf("ddrptr = 0x%08x, DDR_BLOCK_SIZE = 0x%04x\n\r", ddrptr, DDR_BLOCK_SIZE);
-    srand(1);
-    for (int i=0; i<DDR_BLOCK_SIZE/4; i++) ddrptr[i] = rand();
-    errors=0;
-    srand(1);
-    for (int i=0; i<DDR_BLOCK_SIZE/4; i++) {
-    	randval = rand();
-    	readval = ddrptr[i];
-    	if (randval != readval) {
-    		errors++;
-    		xil_printf("%d: %08x  %08x\n\r", i, randval, readval);
-    	}
-    }
-    xil_printf("ddr test: errors = %d\n\r", errors);
     
     xil_printf("testing complete\n\r");
     
